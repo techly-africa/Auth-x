d0558a645117639f293a1c0f1eabefd03c006f93
Ngoga Innocent <ngogainnocent1@gmail.com>
2023-06-02 03:00:32 -0700

    login and token handle

---
+++ src/middleware/authenticated.ts
@@ -0,0 +1,26 @@
+import express from "express";
+import { IVendor } from "helpers/interfaces";
+import Jwt from "jsonwebtoken";
+
+const IsAuthenticated = async (
+  req: express.Request,
+  res: express.Response,
+  next: express.NextFunction
+) => {
+  const token = req.header("token");
+  if (!token) {
+    return res.status(401).json({ msg: "No Token found" });
+  }
+
+  try {
+    const vendor = await Jwt.verify(
+      token,
+      process.env.JWT_SECRET as Jwt.Secret
+    );
+
+    next();
+  } catch (error) {
+    return res.json({ msg: "Invalid Token" });
+  }
+};
+export default IsAuthenticated;
