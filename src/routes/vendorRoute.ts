7fb581579fcf3ab849ffa060db5a21c18c4d1f6e
Faid Terence JABO <faidterence@outlook.com>
2023-09-19 20:01:05 +0200

    Ch(Nestjs): Installation

--- src/routes/vendorRoute.ts
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { AllVendor, Register } from "../controllers/VendorController";
-
-const router = express.Router();
-
-router.post("/register", Register);
-router.get("/vendors", AllVendor);
-
-export default router;
