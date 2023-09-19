6804bb0b17881c4bcf73f9e78092819c66aeed61
Faid Terence JABO <faidterence@outlook.com>
2023-09-19 21:23:54 +0200

    MongoDB Configuration

--- dist/app.module.js
+++ dist/app.module.js
@@ -10,12 +10,17 @@ exports.AppModule = void 0;
 const common_1 = require("@nestjs/common");
 const app_controller_1 = require("./app.controller");
 const app_service_1 = require("./app.service");
+const mongoose_1 = require("@nestjs/mongoose");
+const config_1 = require("@nestjs/config");
 let AppModule = class AppModule {
 };
 exports.AppModule = AppModule;
 exports.AppModule = AppModule = __decorate([
     (0, common_1.Module)({
-        imports: [],
+        imports: [
+            config_1.ConfigModule.forRoot({ isGlobal: true }),
+            mongoose_1.MongooseModule.forRoot(process.env.MONGODB_URL),
+        ],
         controllers: [app_controller_1.AppController],
         providers: [app_service_1.AppService],
     })
