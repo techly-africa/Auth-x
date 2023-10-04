e37e1a5affb3a7c97652cb30516e4f9ee1e3e60e
Terence Faid JABO <faidterence@outlook.com>
2023-10-04 21:59:51 +0200

    feat(auth): Enable verified users to log in

--- src/mail/mail.service.ts
+++ src/mail/mail.service.ts
@@ -13,7 +13,7 @@ export class MailService {
                 to: email,
                 from: '"Techly Africa Support Team" <support@example.com>',
                 subject: 'MPA-CASH Account Verification',
-                text: `Hi ${user},\n\nWelcome to MPA-CASH! We're excited to have you on board. To ensure the security of your account and access all the features of our platform, please verify your email address by clicking the link below:\n\n${verificationLink}\n\nThis link will expire in 24 hours, so please verify your email address as soon as possible.\n\nIf you didn't sign up for MPA-CASH or believe you received this email by mistake, please ignore it. Your account will not be activated until you verify your email.\n\nBest regards,\nTechly Africa Team`,
+                text: `Hi ${user},\n\nWelcome to Techly Africa! We're excited to have you on board. To ensure the security of your account and access all the features of our platform, please verify your email address by clicking the link below:\n\n${verificationLink}\n\nThis link will expire in 24 hours, so please verify your email address as soon as possible.\n\nIf you didn't sign up for MPA-CASH or believe you received this email by mistake, please ignore it. Your account will not be activated until you verify your email.\n\nBest regards,\nTechly Africa Team`,
                 html: `
                     <body style="font-family: Arial, sans-serif; margin: 0; padding: 0; background-color: #f4f4f4;">
                         <div style="background-color: #fff; max-width: 600px; margin: 20px auto; padding: 20px; border-radius: 5px; box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1);">
@@ -21,7 +21,7 @@ export class MailService {
                             <p style="font-size: 16px; color: #555;">Hi ${user},</p>
                             <p style="font-size: 16px; color: #555;">Welcome to MPA-CASH! We're excited to have you on board. To ensure the security of your account and access all the features of our platform, please verify your email address by clicking the link below:</p>
                             <a href="${verificationLink}" style="background-color: #f9f9f9; display: inline-block; padding: 10px; border-radius: 5px; text-align: center; font-size: 24px; font-weight: bold; color: #2678d0; text-decoration: none;">
-                               EmailVerification
+                            Email Confirmation Link
                             </a>
                             <p style="font-size: 16px; color: #555;">This link will expire in 24 hours, so please verify your email address as soon as possible.</p>
                             <p style="font-size: 16px; color: #555;">If you didn't sign up for MPA-CASH or believe you received this email by mistake, please ignore it. Your account will not be activated until you verify your email.</p>
