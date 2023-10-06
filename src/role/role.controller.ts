33df48a4441c7dac8a9dd725cd5c41db6cc64628
Terence Faid JABO <faidterence@outlook.com>
2023-10-06 12:35:34 +0200

    feat: Update and Delete Role

--- src/role/role.controller.ts
+++ src/role/role.controller.ts
@@ -22,17 +22,17 @@ export class RoleController {
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.roleService.findOne(id);
+  async findOne(@Param('id') id: string) {
+    return this.roleService.findOneRole(id);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateRoleDto: UpdateRoleDto) {
+  async update(@Param('id') id: string, @Body() updateRoleDto: UpdateRoleDto) {
     return this.roleService.update(id, updateRoleDto);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.roleService.remove(id);
+  async remove(@Param('id') id: string) {
+    return this.roleService.removeRole(id);
   }
 }
--- src/role/role.service.ts
+++ src/role/role.service.ts
@@ -1,7 +1,7 @@
-import { BadRequestException, Injectable, InternalServerErrorException } from '@nestjs/common';
+import { BadRequestException, Injectable, InternalServerErrorException, NotFoundException } from '@nestjs/common';
 import { CreateRoleDto } from './dto/create-role.dto';
 import { UpdateRoleDto } from './dto/update-role.dto';
-import { Model } from 'mongoose';
+import mongoose, { Model } from 'mongoose';
 import { Role } from './schemas/role.schema';
 import { InjectModel } from '@nestjs/mongoose';
 
@@ -13,27 +13,34 @@ export class RoleService {
     private rolesModel : Model<Role>
   ) {}
 
-  async createRole(role: CreateRoleDto):Promise<{message: string}> {
-    const {roleName, description} = role;
-    if(!roleName || !description) {
-      throw new BadRequestException('Invalid Inputs!');
-    }
+  async createRole(role: CreateRoleDto): Promise<{ message: string }> {
+    const { roleName, description } = role;
+  
     try {
-      const roleExist = await this.rolesModel.findOne({roleName});
-
-      if(roleExist) {
+      // Validate inputs
+      if (!roleName || !description) {
+        throw new BadRequestException('Invalid Inputs!');
+      }
+  
+      // Check if the role already exists
+      const roleExist = await this.rolesModel.findOne({ roleName });
+  
+      if (roleExist) {
         throw new BadRequestException('Role Already Exists');
       }
+  
+      // Create the role
       await this.rolesModel.create({
-        ...role
+        ...role,
       });
-      return {message: 'Role Created Succesful'}
-      
+  
+      return { message: 'Role Created Successfully' };
     } catch (error) {
-      
+      // Handle any errors
+      throw new InternalServerErrorException('Role creation failed', error.message);
     }
   }
-
+  
   async findAllRoles(): Promise<Role[]> {
     try {
       const roles = await this.rolesModel.find()
@@ -45,15 +52,66 @@ export class RoleService {
     }
   }
 
-  findOne(id: string) {
-    return `This action returns a #${id} role`;
+  async findOneRole(id: string): Promise<Role> {
+    try {
+      // Find the role by ID
+      const role = await this.rolesModel.findById(id);
+  
+      // Check if the role exists
+      if (!role) {
+        throw new NotFoundException('Role not found');
+      }
+  
+      // Return the found role
+      return role;
+    } catch (error) {
+      // Handle any errors
+      if (error instanceof mongoose.Error.CastError) {
+        // Handle invalid ID format errors
+        throw new BadRequestException('Invalid role ID format');
+      } else {
+        // Handle other errors
+        throw new InternalServerErrorException('Server Error', error.message);
+      }
+    }
+  }
+  
+  async update(id: string, updateRoleDto: UpdateRoleDto):Promise<Role> {
+    try {
+      const role = await this.rolesModel.findById(id)
+      if(!role) {
+        throw new BadRequestException(`Role with ${id} does not exist`)
+      }
+      if(updateRoleDto.roleName){
+        role.roleName = updateRoleDto.roleName
+      }
+      if(updateRoleDto.description) {
+        role.description = updateRoleDto.description
+      }
+
+      const updatedRole = await role.save();
+
+      return updatedRole
+      
+    } catch (error) {
+      // Handle any errors
+      if (error instanceof mongoose.Error.CastError) {
+        // Handle invalid ID format errors
+        throw new BadRequestException('Invalid role ID format');
+      } else {
+        // Handle other errors
+        throw new InternalServerErrorException('Server Error', error.message);
+      }
+    }
   }
 
-  update(id: string, updateRoleDto: UpdateRoleDto) {
-    return `This action updates a #${id} role`;
-  }
-
-  remove(id: string) {
-    return `This action removes a #${id} role`;
+  async removeRole(id: string):Promise<{message: string}> {
+    const role = await this.rolesModel.findByIdAndDelete(id);
+    if(!role) {
+      throw new BadRequestException(`Role with ${id} does not exist`)
+    }
+    return {
+      message: "Role Deleted Successfully"
+    }
   }
 }
