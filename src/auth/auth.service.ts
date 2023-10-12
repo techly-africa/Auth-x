4e13960a2fe8746228d8c04f2ba26975bf1f03b7
edwinnambaje <nambajeeedwin@gmail.com>
2023-10-12 17:37:31 +0200

    added auth service tests

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -5,7 +5,7 @@ import {
 } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
-import { User } from 'src/user/schemas/user.schema';
+import { User } from '../user/schemas/user.schema';
 import { LoginUserDto } from './login-user.dto';
 import * as bcrypt from 'bcrypt';
 import { JwtService } from '@nestjs/jwt';
@@ -16,7 +16,7 @@ export class AuthService {
     @InjectModel(User.name)
     private userModel: Model<User>,
     private jwtServices: JwtService,
-  ) {}
+  ) { }
 
   async loginUser(
     user: LoginUserDto,
